adc07d2965727383ace2e3143b6f57a24492f8a2
removedporn <86824510+removedporn@users.noreply.github.com>
2023-07-16 21:14:12 +0800

    Delete network directory

--- network/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from network.models import User, Post, Profile
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Profile)
